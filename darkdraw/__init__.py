85007c2bce7b422bcdc2efedc15fd51d2c467e02
Saul Pwanson <code@saul.pw>
2021-12-12 21:07:28 -0800

    simple png saver; no color/attrs/animations

--- darkdraw/__init__.py
+++ darkdraw/__init__.py
@@ -4,6 +4,7 @@ from .drawing import *
 from .stamps import *
 
 from .ansihtml import * # save to .ansihtml
+from .save import *
 
 from .loader_scr import *  # deprecated 2020 format, remove anytime
 
---
+++ darkdraw/save.py
@@ -0,0 +1,41 @@
+from visidata import VisiData
+from .drawing import Drawing, DrawingSheet
+from unittest import mock
+
+from PIL import Image, ImageDraw, ImageFont
+
+@VisiData.api
+def save_png(vd, p, *sheets):
+    im = Image.new("RGB", (640, 480), color=0)
+
+    draw = ImageDraw.Draw(im)
+    font = ImageFont.truetype("/usr/share/fonts/truetype/unifont/unifont.ttf", 16)
+
+    for vs in sheets:
+        if isinstance(vs, DrawingSheet):
+            dwg = Drawing('', source=vs)
+        elif isinstance(vs, Drawing):
+            dwg = vs
+        else:
+            vd.fail(f'{vs.name} not a drawing')
+
+        dwg._scr = mock.MagicMock(__bool__=mock.Mock(return_value=True),
+                                  getmaxyx=mock.Mock(return_value=(9999, 9999)))
+        dwg.draw(dwg._scr)
+
+        displayed = set()
+        for y in range(dwg.minY, dwg.maxY+1):
+            for x in range(dwg.minX, dwg.maxX+1):
+                rows = dwg._displayedRows.get((x,y), None)
+                if not rows: continue
+                r = rows[-1]
+                k = str(r)
+                if k in displayed: continue
+                if not r.text: continue
+                if x-r.x >= len(r.text): continue
+                i = x-r.x
+                s = r.text[i:]
+                draw.text(((r.x+i)*8, r.y*16), s, font=font)
+                displayed.add(k)
+
+    im.save(str(p))
