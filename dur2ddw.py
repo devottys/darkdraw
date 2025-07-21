11b2f7b5cb9ba4092611b161beba6f8d66a52113
AdrianGroty <40133867+AdrianGroty@users.noreply.github.com>
2025-07-21 17:31:11 -0500

    update dur2ddw.py
    Add separate map for bg, include white space, exclude black white space

--- dur2ddw.py
+++ dur2ddw.py
@@ -11,26 +11,37 @@ import sys
 import json
 import gzip
 
-durdraw_color16_map = {
-    0: -1, #??
-    1: 0,
+durdraw_color16_fg_map = {
+        0: -1, #??
+    1: 0, # black
     2: 4, # blue
     3: 2, # green
     4: 6, # cyan
     5: 1, # red
     6: 5, # magenta
     7: 3, # yellow
-    8: 7,
-    9: 0,
+    8: 7, # light grey
+    9: 8, # dark grey
     10: 12, # bright blue
     11: 10, # bright green
     12: 14, # bright cyan
     13: 9, # bright red
     14: 13, # bright magenta
     15: 11, # bright yellow
-    16: 15,
+    16: 15, # white
 }
 
+durdraw_color16_bg_map = {
+        0: -1, #??
+    1: 4, # blue
+    2: 2, # green
+    3: 6, # cyan
+    4: 1, # red
+    5: 5, # magenta
+    6: 3, # yellow
+    7: 7, # light grey
+    8: 0, # black
+}
 
 def convert_dur_to_ddw(infn, outfp):
     dur = json.loads(gzip.open(infn).read())
@@ -49,11 +60,11 @@ def convert_dur_to_ddw(infn, outfp):
 
         for y, line in enumerate(lines):
             for x, ch in enumerate(line):
-                if ch == ' ':
-                    continue
                 fg, bg = colors[x][y]
-                fg = durdraw_color16_map[fg]
-                bg = durdraw_color16_map[bg]
+                fg = durdraw_color16_fg_map[fg]
+                bg = durdraw_color16_bg_map[bg]
+                if ch == ' ' and bg == 0:
+                    continue
                 d = dict(x=x,
                          y=y,
                          text=ch,
