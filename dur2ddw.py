042a3113cc6c26e08e81a7bebf577849a4370cb6
Saul Pwanson <code@saul.pw>
2025-12-05 15:16:31 -0800

    [dur2ddw] cleanup

--- dur2ddw.py
+++ dur2ddw.py
@@ -11,51 +11,48 @@ import sys
 import json
 import gzip
 
+durdraw_color16_fg_map = {
+    0: 0, # black
+    1: 0, # also black
+    2: 4, # blue
+    3: 2, # green
+    4: 6, # cyan
+    5: 1, # red
+    6: 5, # magenta
+    7: 3, # yellow
+    8: 7, # light grey
+    9: 8, # dark grey
+    10: 12, # bright blue
+    11: 10, # bright green
+    12: 14, # bright cyan
+    13: 9, # bright red
+    14: 13, # bright magenta
+    15: 11, # bright yellow
+    16: 15, # white
+}
+
+durdraw_color16_bg_map = {
+    0: 0, # black
+    1: 4, # blue
+    2: 2, # green
+    3: 6, # cyan
+    4: 1, # red
+    5: 5, # magenta
+    6: 3, # yellow
+    7: 7, # light grey
+    8: 0, # also black
+}
+
 def convert_dur_to_ddw(infn, outfp):
     dur = json.loads(gzip.open(infn).read())
-
-    if dur['DurMovie']['colorFormat'] == '16':
-        durdraw_color16_fg_map = {
-            0: 0, # black
-            1: 0, # also black
-            2: 4, # blue
-            3: 2, # green
-            4: 6, # cyan
-            5: 1, # red
-            6: 5, # magenta
-            7: 3, # yellow
-            8: 7, # light grey
-            9: 8, # dark grey
-            10: 12, # bright blue
-            11: 10, # bright green
-            12: 14, # bright cyan
-            13: 9, # bright red
-            14: 13, # bright magenta
-            15: 11, # bright yellow
-            16: 15, # white
-        }
-
-        durdraw_color16_bg_map = {
-            0: 0, # black
-            1: 4, # blue
-            2: 2, # green
-            3: 6, # cyan
-            4: 1, # red
-            5: 5, # magenta
-            6: 3, # yellow
-            7: 7, # light grey
-            8: 0, # also black
-        }
 
     for f in dur['DurMovie']['frames']:
         n = f['frameNumber']
         lines = f['contents']
         colors = f['colorMap']
-        ### if delay is not specified, find duration based on animation framerate
-        if f['delay'] == 0: 
+        if f['delay'] == 0: ### if delay is not specified, find duration based on animation framerate
             duration_ms = int(1000 // dur['DurMovie']['framerate'])
-        ### if specified, convert to ms
-        else: 
+        else: ### if specified, convert to ms
             duration_ms = int(f['delay'] * 1000)
 
         d = dict(
@@ -71,6 +68,8 @@ def convert_dur_to_ddw(infn, outfp):
                 if dur['DurMovie']['colorFormat'] == '16':
                     fg = durdraw_color16_fg_map[fg]
                     bg = durdraw_color16_bg_map[bg]
+                # else use the standand 256 color mapping numbers as they are
+
                 if ch == ' ' and bg == 0:
                     continue
                 d = dict(x=x,
